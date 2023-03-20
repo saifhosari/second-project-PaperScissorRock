d6acd91caa664ac4c3b2881171f05496d307b7a1
Saif Hosari <saifhalab93@gmail.com>
2023-03-20 17:24:09 +0000

    function incrementScore(scoreSpan){

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const buttons= document.querySelectorAll("[data-selection]");
 const finalColumn = document.querySelector("[data-final-column]")
+const zombieScoreSpan = document.querySelector("[data-zombie-score]")
+const yourScoreSpan = document.querySelector("[data-your-score]")
 const CHOICES = [
 {
     name:"rock",
@@ -36,9 +38,13 @@ const makeSelection = (selection)=>{
     addSelectionResult(selection,youWinner)
 }
 
+function incrementScore(scoreSpan){
+scoreSpan.innerText = parseInt(scoreSpan.innerText)
+}
+
 function addSelectionResult(selection,winner){
     const div = document.createElement("div")
-    div.innerText = (selection.emoji)
+    div.innerText = selection.emoji
     div.classList.add("result-selection")
     if(winner) {
         div.classList.add("winner")
